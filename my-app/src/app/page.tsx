d8cd3a1c48bda3740b553cc9d044446cedf76181
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2024-09-09 09:57:16 -0500

    updates Group import, vercel error

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { PrismaClient, Group } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
+import type { Group } from "@prisma/client"; // Import the 'group' type from Prisma
 
 const prisma = new PrismaClient();
 
 async function getGroups(): Promise<Group[]> {
+  // Ensure the return type is 'group[]'
   const groups = await prisma.group.findMany();
   return groups;
 }
@@ -14,11 +16,16 @@ export default async function Home() {
 
   return (
     <main>
-      {groups.map((item: Group, i: number) => (
-        <div key={i}>
-          <p>{item.name}</p>
-        </div>
-      ))}
+      {groups.map(
+        (
+          item: Group,
+          i: number // Explicitly define the type for 'item'
+        ) => (
+          <div key={i}>
+            <p>{item.name}</p>
+          </div>
+        )
+      )}
     </main>
   );
 }
