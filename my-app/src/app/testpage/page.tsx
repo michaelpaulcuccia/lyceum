b0261c3264f60c0db962ac0e29dd9616e7662ad8
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2024-09-10 11:44:35 -0500

    nice styling

--- my-app/src/app/testpage/page.tsx
+++ my-app/src/app/testpage/page.tsx
@@ -25,28 +25,68 @@ const DarkOverlay = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  background: rgba(0, 0, 0, 0.2); /*  20% opacity */
+  background: rgba(0, 0, 0, 0.35); /*  35% opacity */
   z-index: 2; /* Overlay stays below the text */
   pointer-events: none; /* Ensures overlay does not block text or other elements */
 `;
 
 const TextOverlay = styled.div`
   position: absolute;
+  top: 0;
+  left: 0;
+  padding: 20px 60px;
+  width: 100%;
   color: white;
   z-index: 3; /* Text stays on top of everything */
+`;
+
+const TopTextNav = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  h1 {
+    font-size: 56px;
+    line-height: 56px;
+  }
+`;
+
+const LinkWrap = styled.div`
+  display: flex;
+
+  div {
+    font-size: 20px;
+    line-height: 20px;
+    margin-right: 18px;
+  }
 `;
 
 export default function BackgroundImageWithText() {
   return (
     <FullPageContainer>
       <DarkenedImage
-        src="/images/reading_002.jpg"
+        src="/images/library_001.jpg"
         alt="Background Image"
         layout="fill" /* Important for full coverage */
       />
       <DarkOverlay />
       <TextOverlay>
-        <h1>Your Text Here</h1>
+        <TopTextNav>
+          <h1>Lyceum</h1>
+          <LinkWrap>
+            <div>Who</div>
+            <div>What</div>
+            <div>When</div>
+            <div>Where</div>
+          </LinkWrap>
+          <LinkWrap>
+            <div>Login</div>
+          </LinkWrap>
+          <LinkWrap>
+            <div>IG</div>
+            <div>Tube</div>
+          </LinkWrap>
+        </TopTextNav>
       </TextOverlay>
     </FullPageContainer>
   );
