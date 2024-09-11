73c141ebfc3b15f860288c30239a3af7b67e1ce6
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2024-09-11 12:58:44 -0500

    heroData obj

--- my-app/constants/index.ts
+++ my-app/constants/index.ts
@@ -1,1 +1,64 @@
 export const mobileBreakpoint = "865px";
+
+export const heroData = [
+  {
+    directory: "/images/book_001.jpg",
+    headlines: [
+      "Unfold New Worlds Together",
+      "Discover Stories, Share Moments",
+      "Pages Turning, Minds Growing",
+      "Open Books, Open Minds",
+      "Read, Reflect, Connect",
+    ],
+  },
+  {
+    directory: "/images/book_002.jpg",
+    headlines: [
+      "See Through the Lens of Stories",
+      "Knowledge Awaits, Let's Explore",
+      "Pause, Read, Reflect",
+      "The Art of Quiet Moments",
+      "Find Your Next Great Read",
+    ],
+  },
+  {
+    directory: "/images/library_001.jpg",
+    headlines: [
+      "Journey Through the Stacks",
+      "Find Inspiration in Every Aisle",
+      "Explore New Ideas, One Page at a Time",
+      "Where Stories Come to Life",
+      "Meet, Read, and Discover",
+    ],
+  },
+  {
+    directory: "/images/library_002.jpg",
+    headlines: [
+      "A World of Books Awaits",
+      "Shelf Full of Possibilities",
+      "Books Bring Us Together",
+      "Find Your Next Adventure Here",
+      "Explore. Borrow. Share.",
+    ],
+  },
+  {
+    directory: "/images/reading_001.jpg",
+    headlines: [
+      "Get Lost in a Good Book",
+      "The Comfort of a Great Story",
+      "Relax and Read Together",
+      "Quiet Moments, Shared Joy",
+      "Find Your Reading Spot",
+    ],
+  },
+  {
+    directory: "/images/reading_002.jpg",
+    headlines: [
+      "Serenity Meets Storytelling",
+      "Read by the Water’s Edge",
+      "Find Peace in the Pages",
+      "Books and Tranquility Combined",
+      "Reflect, Read, and Connect",
+    ],
+  },
+];
